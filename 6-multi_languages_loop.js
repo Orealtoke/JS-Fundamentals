8c8c4644198355ce64f6bc008559413192fea184
Oreal <khadijatoritoke0@gmail.com>
2025-07-13 14:50:19 +0100

    Resolved the changes in 6-multi_languages_loop.js file

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-const lines = ['C is fun', 'Python is cool', 'JvaScript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const line of lines) {
     console.log(line);
 }
